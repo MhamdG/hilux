1359f41feb396296760cf8d619d9f53c394cce32
Mohammed A Alghoul <mhamdg@outlook.com>
2023-09-13 14:10:26 +0400

    Revert "search page 2"
    This reverts commit 006f7eeae3330241ef4ad76b037121af53b5f4e2.

--- src/app/rate/pricingUnit/pricingUnit-search.component.ts
+++ src/app/rate/pricingUnit/pricingUnit-search.component.ts
@@ -134,7 +134,7 @@ export class PricingUnitComponent implements OnInit {
         distinctUntilChanged(),
         tap(() => this.projectNameOptionsLoading = true),
         switchMap(term => {
-          return this.lookupsService.loadAllProjects({ term }).pipe(
+          return this.lookupsService.loadAllProjects({ term, developerId: this.searchData.searchDeveloperId }).pipe(
             catchError(() => of([])), // empty list on error
             tap(() => this.projectNameOptionsLoading = false)
           )
