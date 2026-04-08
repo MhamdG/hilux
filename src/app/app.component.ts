c0b5e430a47f90a6bed24727d0cd654fb64a3295
Mohammed Alghoul <mhamdg@outlook.com>
2026-04-08 10:45:05 +0400

    feat: implement responsive sidebar collapse, and add table responsiveness to signature collection field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, Renderer2 } from '@angular/core';
 import bsCustomFileInput from 'bs-custom-file-input';
 
 @Component({
@@ -6,10 +6,27 @@ import bsCustomFileInput from 'bs-custom-file-input';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  implements OnInit {
+export class AppComponent implements OnInit {
   title = 'hilux';
 
+  constructor(private renderer: Renderer2) {}
+
   ngOnInit() {
-    bsCustomFileInput.init()
+    bsCustomFileInput.init();
+    this.checkSidebar();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkSidebar();
+  }
+
+  private checkSidebar() {
+    if (window.innerWidth < 992) {
+      this.renderer.addClass(document.body, 'sidebar-collapse');
+      this.renderer.removeClass(document.body, 'sidebar-open');
+    } else {
+      this.renderer.removeClass(document.body, 'sidebar-collapse');
+    }
   }
 }
